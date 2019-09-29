a067ab18f7017dd10648ceff12d29c8767795c1c
Viktor Rozenko <sharp.vik@gmail.com>
2019-09-29 16:17:37 -0400

    script improvements

--- web/script.js
+++ web/script.js
@@ -1,32 +1,8 @@
 document.querySelector('#clear').addEventListener('click', function() {
     document.querySelector('#exp').innerHTML = '';
 });
-document.querySelector('#lbrace').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '(';
-});
-document.querySelector('#rbrace').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += ')';
-});
-document.querySelector('#div-sign').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '÷';
-});
-document.querySelector('#mul-sign').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '×';
-});
-document.querySelector('#sub-sign').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '−';
-});
-document.querySelector('#add-sign').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '+';
-});
-document.querySelector('#pow-sign').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '^';
-});
-document.querySelector('#dot-sign').addEventListener('click', function() {
-    document.querySelector('#exp').innerHTML += '.';
-});
 document.addEventListener('click', function(e) {
-    if ( !e.target.matches('.digit') ) return;
+    if ( !e.target.matches('.digit') && !e.target.matches('.sign') ) return;
     document.querySelector('#exp').innerHTML += e.target.innerHTML;
 }, false);
 document.querySelector('#eql-sign').addEventListener('click', function() {
